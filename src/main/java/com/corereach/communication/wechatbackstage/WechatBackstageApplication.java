e67548a9a3d1a8999279dfc09584f7a77dd728af
ga.zhang <1299407509@qq.com>
2019-12-23 14:41:30 +0800

    update

--- src/main/java/com/corereach/communication/wechatbackstage/WechatBackstageApplication.java
+++ src/main/java/com/corereach/communication/wechatbackstage/WechatBackstageApplication.java
@@ -2,11 +2,13 @@ package com.corereach.communication.wechatbackstage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author ga.zhang
  */
 @SpringBootApplication
+@ComponentScan({"org.n3r.idworker"})
 public class WechatBackstageApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/corereach/communication/wechatbackstage/comm/ChatCode.java
@@ -0,0 +1,11 @@
+package com.corereach.communication.wechatbackstage.comm;
+
+/**
+ * @Description: TODO
+ * @Author ga.zhang
+ * @Date 2019/12/23 13:40
+ * @Version V1.0
+ **/
+public class ChatCode {
+
+}
